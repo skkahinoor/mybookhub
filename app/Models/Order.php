5871af5220dd48a28208a7acaa0021f9313ea6ee
Biswajit Mohanta <rb486373@gmail.com>
2026-01-08 16:01:21 +0530

    Update fillable

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,6 +9,48 @@ class Order extends Model
 {
     use HasFactory;
 
+    protected $table = 'orders';
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'address',
+        'city',
+        'state',
+        'country',
+        'pincode',
+        'mobile',
+        'email',
+        'shipping_charges',
+        'coupon_code',
+        'coupon_amount',
+        'order_status',
+        'payment_method',
+        'payment_gateway',
+        'grand_total',
+        'courier_name',
+        'tracking_number',
+        'is_pushed',
+    ];
+
+    protected $casts = [
+        'user_id'           => 'integer',
+        'shipping_charges'  => 'float',
+        'coupon_amount'     => 'float',
+        'grand_total'       => 'float',
+        'is_pushed'         => 'boolean',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function logs()
+    {
+        return $this->hasMany(OrdersLog::class, 'order_id');
+    }
+
     // Note: For the Orders module, we created two database tables: `orders` and `orders_products` tables. The first one holds/stores the main information about the orders of a user (e.g. delivery address, coupon code, shipping, payment method, ...etc), and the second one holds/stores the detailed information about the order (the items/products that are bought by the order and product name, code, color, size, price, ...etc). There is a one-to-many relationship between the two tables where one order can have many order products.
 
 
