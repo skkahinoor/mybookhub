88696f185dcc8a60ff488b55836812603ed5a093
skkahinoor <skkahinoor23@gmail.com>
2026-01-14 18:11:31 +0530

    working on vendor

--- app/Http/Middleware/Vendor.php
+++ app/Http/Middleware/Vendor.php
@@ -11,9 +11,10 @@ class Vendor
     public function handle(Request $request, Closure $next)
     {
         if (!Auth::check()) {
-            return redirect()->route('vendor.login');
+            return redirect('/vendor/login');
+        }
 
-        if (Auth::guard('vendor')->user()->type !== 'vendor') {
+        if (Auth::guard('admin')->user()->type !== 'vendor') {
             abort(403, 'Vendor access only.');
         }
 
