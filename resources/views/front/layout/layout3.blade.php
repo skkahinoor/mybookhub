7aaeb307ebb16fe61406252793b2ef2a0fa56d0e
Biswajeet Mohanta <rb486373@gmail.com>
2026-01-22 11:47:11 +0530

    Fix infinite loader issue

--- resources/views/front/layout/layout3.blade.php
+++ resources/views/front/layout/layout3.blade.php
@@ -767,7 +767,7 @@
     <script src="{{ asset('front/newtheme/js/custom.js') }}"></script><!-- CUSTOM JS -->
 
     <!-- jQuery -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+{{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> --}}
 
 <!-- Bootstrap JS -->
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
